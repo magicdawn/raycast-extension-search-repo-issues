7aa8ee42ea10d70ca94c21d5db41cdf625aa8f4e
Tao <magicdawn@qq.com>
2023-03-05 01:26:50 +0800

    feat: rm proxy settings

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,5 @@
 import { getPreferenceValues, showToast, Toast } from '@raycast/api'
-import { Agent } from 'http'
 import { Octokit } from 'octokit'
-import ProxyAgent from 'proxy-agent-tweak'
 
 export function toastError(message: string, title = 'Error') {
   return showToast({
@@ -14,23 +12,17 @@ export function toastError(message: string, title = 'Error') {
 interface Preferences {
   repo: string
   ghToken?: string
-  httpProxy?: string
 }
 
 const pref = getPreferenceValues<Preferences>()
-const { repo, ghToken, httpProxy } = pref
+const { repo, ghToken } = pref
 console.log('[pref] = %O', pref)
 
 export const REPO = repo || ''
 
-let agent: Agent | undefined
-if (httpProxy) {
-  agent = ProxyAgent(httpProxy)
-}
-
 export const octokit = new Octokit({
   auth: ghToken || undefined,
   request: {
-    agent,
+    // agent,
   },
 })
